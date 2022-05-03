d13d892d0453ab5a3a0eeb2a3aaf2fb0acc5307c
Jaskaran Singh <singhjaskaran975@gmail.com>
2022-05-03 11:55:06 +0530

    change

--- eCommerceApp/products/src/components/Footer.jsx
+++ eCommerceApp/products/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <div className="footer">
-            Micro Frontends Demo (Shopping application designed with Module Federation)
+            Micro Frontends Demo
         </div>
     );
 }
