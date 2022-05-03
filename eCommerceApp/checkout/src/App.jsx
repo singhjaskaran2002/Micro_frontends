2833dee05014b57044fbf84ec36e553eddae3aa1
Jaskaran Singh <singhjaskaran975@gmail.com>
2022-05-03 17:51:08 +0530

    added checkout route

--- eCommerceApp/checkout/src/App.jsx
+++ eCommerceApp/checkout/src/App.jsx
@@ -1,7 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Checkout from './Checkout';
+import './index.css';
 
-import "./index.css";
-
-const App = () => <></>;
-ReactDOM.render(<App />, document.getElementById("app"));
+const App = () => (
+    <Router>
+        <Switch>
+            <Route path="/checkout/:id" component={Checkout} />
+        </Switch>
+    </Router>
+);
+ReactDOM.render(<App />, document.getElementById('app'));
